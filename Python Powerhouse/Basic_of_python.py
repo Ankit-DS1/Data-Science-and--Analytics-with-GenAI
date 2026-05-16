85d0c016fb0b04e85ed35173467c43bd8fcf1bf1
Ankit <ankitku402@gmail.com>
2026-05-16 20:56:07 +0530

    1

---
+++ Python Powerhouse/Basic_of_python.py
@@ -0,0 +1,78 @@
+                     # Class - 2
+
+print("Ankit Kumar")
+#i am ankit kuamr
+
+
+#Variable 
+
+a = 10
+b = 34
+
+Ankit = 34
+Rishab = 1
+
+# Name conversion in varable 
+#3 type of name conversion in varable 
+#1. Camel Case
+#2. Snake Case
+#3. Pascal Case
+
+# Camel Case
+myNameIsAnkit = "Ankit Kumar"
+# Snake Case
+my_name_is_ankit = "Ankit Kumar"
+# Pascal Case
+MyNameIsAn = "Ankit Kumar"
+
+# Data types in python 
+#1. Number - Integer (1,3,5), Float (1.5, 3.7), Complex (1+2j)
+#2. String - use to store anything in " " or ' '
+#3. Boolean - True or False
+
+                        # Class - 3
+
+# In  Depth string in python
+a = "Ankit Kumar"
+print(a[0]) 
+print(a[1:5:3])
+
+#Type Conversion in python 
+
+# int() - convert the datatype to integer 
+#float() - convert the datatype to float
+#str() - convert the datatype to string
+#bool() - convert the datatype to boolean
+a = 10
+b = 3.14
+c = "Ankit Kumar"
+print(int(b)) # 3
+print(float(a)) # 10.0
+print(str(a)) # "10"
+print(bool(a)) # True
+print(bool(0)) # False
+
+age = 23 
+Des = "Data scientist"
+print(f"My name is Ankit Kumar, I am {age} years old and I am a {Des}")
+
+
+
+a = 0
+print(bool(a)) # False
+
+age = float(input("Enter your age: "))
+print(type(age))
+
+
+#Q1 You are given as input. Your task is to reverse the string using python's slicing feature and print the reversied string.
+
+a = input("Enter a string: ")
+
+print(a[::-1])
+
+# Q2 You are given a string as input. Your task is to print the last character of the string.
+
+a = input("Enter a string: ")
+
+print(a[-1])
---
+++ Python Powerhouse/Operator_in_python.py
@@ -0,0 +1,64 @@
+                        #class 4
+
+# Operators in python
+
+# 1. Arithmetic operators
+
+# +, -, *, /, %, **, //
+
+a = 10
+b = 3
+print(a + b)  # 13
+print(a - b)  # 7
+print(a * b)  # 30
+print(a / b)  # 3.333...
+print(a % b)  # 1
+print(a ** b) # 1000
+print(a // b) # 3
+
+#Assignment operators/shorthand operators
+# =, +=, -=, *=, /=, %=, **=, //=
+
+a = 10
+a += 5  # a = a + 5
+print(a)  # 15
+
+# Concatenation operator
+str1 = "Hello"
+str2 = "World"
+result = str1 + " " + str2
+print(result)  # Hello World
+
+#Multiplication operator
+str3 = "Python"
+result = str3 * 3
+print(result)  # PythonPythonPython
+
+
+                              # Comparison operators and Logical operators
+
+
+# Comparison operators
+# ==, !=, >, <, >=, <=
+
+x = 5
+y = 10
+print(x == y)  # False
+print(x != y)  # True
+print(x > y)   # False
+print(x < y)   # True
+print(x >= y)  # False
+print(x <= y)  # True
+
+# Logical operators
+
+# and, or, not
+
+a = True
+b = False
+print(a and b)  # False , because both a and b need to be True for the result to be True
+print(a or b)   # True , because at least one of a or b is True
+print(not a)    # False , because a is True and not True is False
+
+
+
