9e541a284a48d378eb28bddaa31f860ed27f5b84
Ankit <akanksha11683@gmail.com>
2026-06-10 07:48:07 +0530

    18th

---
+++ Python Powerhouse/8.OOPs/class_in_python.py
@@ -0,0 +1,18 @@
+
+# Class in python 
+  # -> Class are just like a blueprint of prototype what you wanna make in python
+
+# Attributes and Methods
+
+# A variable stored inside a class is Attribute.
+# A function stored inside a class is method.
+
+class Factory:
+    a = 12
+    def hello():
+        print("How are you")
+
+    print("I am getting initialized")
+
+print(Factory.a)
+Factory.hello()
---
+++ Python Powerhouse/8.OOPs/intro_oops.py
@@ -0,0 +1,24 @@
+                           # OOPs
+
+# Imperative Programming 
+
+a = 12
+b = 13
+
+print(a+b)
+
+# Functional Programming
+
+def addition(a,b):
+    return a + b
+
+print(addition(12,23))
+print(addition(92,63))
+
+#OOps 
+
+
+
+
+
+    
